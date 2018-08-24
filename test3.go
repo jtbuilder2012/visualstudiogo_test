3e62fa46a175edf855a81de13dbe610dfaa7736d
jtbuilder2012 <jntlhome@ntlworld.com>
2018-08-24 19:45:12 +0100

    Push changes to test3

--- test3.go
+++ test3.go
@@ -8,8 +8,5 @@ func main() {
 	fmt.Println("Hello, playground in MS Visual Code first run......")
 	fmt.Println("..and hello again...")
 	fmt.Println("Lest trt this.. one... more time to auto builf on save")
-<<<<<<< HEAD
 	fmt.Println("..and hello again...")
-=======
->>>>>>> b89ee5e86480bada8faaee289e5ec4ece3ca7755
 }
