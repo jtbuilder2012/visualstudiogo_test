b89ee5e86480bada8faaee289e5ec4ece3ca7755
jtbuilder2012 <jntlhome@ntlworld.com>
2018-08-24 19:18:15 +0100

    Create test3.go
    Initial version of the file

---
+++ test3.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, playground in MS Visual Code first run......")
+	fmt.Println("..and hello again...")
+	fmt.Println("Lest trt this.. one... more time to auto builf on save")
+}
