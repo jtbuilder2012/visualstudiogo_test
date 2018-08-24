a47c3ce1bf93e3a0d799a8ccd9db0f11ad3f4ab8
jtbuilder2012 <jntlhome@ntlworld.com>
2018-08-24 19:33:10 +0100

    Compiled and tested test1/2/3

--- test2.go
+++ test2.go
@@ -8,4 +8,5 @@ func main() {
 	fmt.Println("Hello, playground in MS Visual Code first run......")
 	fmt.Println("..and hello again...")
 	fmt.Println("Lest trt this.. one... more time to auto builf on save")
+	fmt.Println("..and hello again...")
 }
